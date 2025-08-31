9d07707684a82ef786b0941d79b45cee24523569
EkaterinaNattrass <cattiva2012@gmail.com>
2025-08-31 20:15:25 +0200

    added routing to a new rigs page

--- src/client/views/main/main.jsx
+++ src/client/views/main/main.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import {Heading, Page, Spacer} from '@oliasoft-open-source/react-ui-library';
-import {Sites} from 'client/components/sites/sites';
-import {OilRigs} from 'client/components/oil-rigs/oil-rigs';
+import React from "react";
+import { Link } from "react-router-dom";
+import { Page, Button } from "@oliasoft-open-source/react-ui-library";
+import { Sites } from "client/components/sites/sites";
+import { OilRigs } from "client/components/oil-rigs/oil-rigs";
 
 export const Main = ({}) => {
   return (
     <Page left={0}>
-      <Heading top>Hiring Challenge</Heading>
-      <Sites/>
-      <Spacer />
-      <OilRigs/>
+      <Sites />
+      <Link to="/rigs">
+        <Button label="Rigs" width={200} />
+      </Link>
     </Page>
   );
 };
